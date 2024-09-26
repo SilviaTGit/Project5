2bd3505889841bfe479f19dfaeabd669e8c3d775
SilviaTGit <163752329+SilviaTGit@users.noreply.github.com>
2024-09-26 23:44:09 +0200

    Fixing the arrow icon and the collapse component

--- kasa/src/components/Collapse.jsx
+++ kasa/src/components/Collapse.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useState } from "react";
+import PropTypes from "prop-types";
 import "../assets/styles/components/_collapse.scss";
-import arrowIcon from '../assets/images/arrowIcon.svg';
+import arrowIcon from "../assets/images/arrow-icon.svg";
 
 const Collapse = ({ title, children }) => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);  // Cambia lo stato di visibilità
-  };
+    const toggle = () => {
+        setIsOpen(!isOpen); // Cambia lo stato di visibilità
+    };
 
-  return (
-    <div className="collapse-container">
-      <button className="collapse-title" onClick={toggle}>
-        {title}
-        <img
-            src={arrowIcon}
-            alt="Flèche"
-            className={`arrow-icon ${isOpen ? 'rotate' : ''}`}
-        />
-      </button>
-      <div className={`collapse-content ${isOpen ? 'open' : 'closed'}`}>
-        {children}
-      </div>
-    </div>
-  );
+    return (
+        <div className="collapse-container">
+            <button className="collapse-title" onClick={toggle}>
+                {title}
+                <img src={arrowIcon} alt="Flèche" className={`arrow-icon ${isOpen ? "rotate" : ""}`} />
+            </button>
+            <div className={`collapse-content ${isOpen ? "open" : "closed"}`}>{children}</div>
+        </div>
+    );
 };
 Collapse.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 export default Collapse;
